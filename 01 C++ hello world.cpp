007de6f78bbba2ac7daa0cf63bd44d1f9dbe40a7
wenchaozheng <61487199+wenchaozheng@users.noreply.github.com>
2022-05-27 18:12:16 +0800

    Add files via upload

---
+++ 01 C++ hello world.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+//单行注释
+
+/*多行注释*/
+
+/*
+	main是一个程序的入口
+	每个程序都有
+	有且仅有一个
+*/
+
+int main1()
+{
+	//11行输出hello world
+	cout << "hello world" << endl;
+
+	system("pause");
+
+	return 0;
+}
